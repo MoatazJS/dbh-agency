1151f395c3ee3e11614da9f20c56d34be5c78d11
unknown <moatazmamdouh.js@gmail.com>
2025-12-16 16:54:55 +0200

    creating layout of about section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HomePage from "@/app/(pages)/HomePage/page";
 import { Navbar } from "@/lib/Components/LayoutComponents/Navbar";
+import AboutSection from "@/lib/Components/PagesComponents/HomeComponents/AboutSection";
 export default function Home() {
   return (
     <>
       <Navbar />
       <HomePage />
+      <AboutSection />
     </>
   );
 }
--- lib/Components/PagesComponents/HomeComponents/AboutSection.tsx
+++ lib/Components/PagesComponents/HomeComponents/AboutSection.tsx
@@ -1,10 +1,20 @@
 "use client";
+import { ParticlesBackground } from "@/components/Particles";
 import React from "react";
 
 export default function AboutSection() {
   return (
-    <div className="min-h-screen text-center text-7xl text-yellow-500">
-      AboutSection
-    </div>
+    <section className="py-32 relative overflow-hidden min-h-screen">
+      <ParticlesBackground />
+      <div className="container mx-auto px-4 md:px-6">
+        <div className="grid md:grid-cols-2 gap-16 items-center">
+          <div>
+            <h2>tata</h2>
+            <h2>tatata</h2>
+          </div>
+          <div>image</div>
+        </div>
+      </div>
+    </section>
   );
 }
