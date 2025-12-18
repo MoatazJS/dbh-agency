088a65bb149d83eff4e2345d64336a36b41197c1
unknown <moatazmamdouh.js@gmail.com>
2025-12-18 14:48:27 +0200

    created and finished the CTA section of about page

--- app/(pages)/About/page.tsx
+++ app/(pages)/About/page.tsx
@@ -3,12 +3,17 @@ import React from "react";
 import AboutHero from "@/lib/Components/PagesComponents/AboutComponents/AboutHero";
 import AboutCards from "@/lib/Components/PagesComponents/AboutComponents/AboutCards";
 import AboutPhilosophy from "@/lib/Components/PagesComponents/AboutComponents/AboutPhilosophy";
+import AboutCTA from "@/lib/Components/PagesComponents/AboutComponents/AboutCTA";
+import LogoSection from "@/lib/Components/PagesComponents/HomeComponents/LogoSection";
 
 export default function About() {
     return (
         <>
             <AboutHero />
+            <LogoSection />
             <AboutCards />
             <AboutPhilosophy />
+            <LogoSection />
+            <AboutCTA />
         </>)
 }
--- lib/Components/PagesComponents/AboutComponents/AboutCTA.tsx
+++ lib/Components/PagesComponents/AboutComponents/AboutCTA.tsx
@@ -1,8 +1,12 @@
-import { motion } from "framer-motion";
+"use client"
+import { motion, useScroll, useTransform } from "framer-motion";
 import Link from "next/link";
 import React from "react";
 
 export default function AboutCTA() {
+    const { scrollYProgress } = useScroll();
+    const scale = useTransform(scrollYProgress, [0, 1], [1, 1.2]);
+    const rotate = useTransform(scrollYProgress, [0, 1], [0, 10]);
     return (
         <>
             <section className="h-[80vh] flex items-center justify-center relative overflow-hidden">
@@ -16,7 +20,7 @@ export default function AboutCTA() {
                         whileHover={{ scale: 1.1 }}
                         whileTap={{ scale: 0.9 }}
                     >
-                        <Link href="/#contact" className="text-4xl md:text-6xl font-bold bg-white text-black px-12 py-6 rounded-full hover:bg-primary transition-colors">
+                        <Link href="/Contact" className="text-4xl md:text-6xl font-bold bg-white text-black px-12 py-6 rounded-full hover:bg-primary transition-colors">
                             LET'S TALK
                         </Link>
                     </motion.div>
