972b92a2b3679ca574eb67a532a8991613fcd67a
unknown <moatazmamdouh.js@gmail.com>
2025-12-18 15:51:43 +0200

    finished layout of hero section of vision page

---
+++ app/(pages)/Vision/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import VisionHero from "@/lib/Components/PagesComponents/VisionComponents/VisionHero"
+import React from "react"
+
+export default function Vision() {
+    return (
+        <>
+            <VisionHero />
+        </>
+    )
+}
