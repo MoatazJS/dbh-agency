31b7b94fd97ba1ed08e2f44a3dee527e5acb667b
unknown <moatazmamdouh.js@gmail.com>
2025-12-18 16:21:53 +0200

    created Method page and hero section files

---
+++ app/(pages)/Method/page.tsx
@@ -0,0 +1,9 @@
+import MethodHero from "@/lib/Components/PagesComponents/MethodComponents/MethodHero";
+import React from "react";
+export default function MethodPage() {
+    return (
+        <>
+            <MethodHero />
+        </>
+    );
+}
