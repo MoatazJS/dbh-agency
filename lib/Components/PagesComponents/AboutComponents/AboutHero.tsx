d06255c48eff2287d570c591e3fa71377e977bc1
unknown <moatazmamdouh.js@gmail.com>
2025-12-18 12:57:51 +0200

    created aboutHero section

---
+++ lib/Components/PagesComponents/AboutComponents/AboutHero.tsx
@@ -0,0 +1,9 @@
+export default function AboutHero() {
+    return (
+        <>
+            <section className="relative h-screen flex items-center justify-center overflow-hidden">
+                <div className="absolute inset-0 bg-black/20 z-0"></div>
+            </section>
+        </>
+    )
+}
