1ced6d8498d69dab48534b464eaa212a66b57248
unknown <moatazmamdouh.js@gmail.com>
2025-12-21 14:18:56 +0200

    creating context provider for loading status

--- lib/Context/LoadingContext.tsx
+++ lib/Context/LoadingContext.tsx
@@ -1,1 +1,18 @@
-"use client"
+import { createContext, useContext, useState } from "react";
+
+const LoadingContext = createContext({
+    isLoading: true,
+    setIsLoading: (_: boolean) => { },
+});
+
+export const LoadingProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    return (
+        <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+            {children}
+        </LoadingContext.Provider>
+    );
+};
+
+export const useLoading = () => useContext(LoadingContext);
