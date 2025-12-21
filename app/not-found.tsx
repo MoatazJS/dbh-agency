bc4f5583b7af95927b2bed6311c490f62603522b
unknown <moatazmamdouh.js@gmail.com>
2025-12-21 13:48:16 +0200

    finished layout of not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFound() {
+
+    return (<>
+
+        <main>
+            <div>Background</div>
+            <div>Content</div>
+        </main>
+    </>)
+}
