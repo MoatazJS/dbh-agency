7f13dab439104e04aa297ad5cc3fdd66b96b4b4e
unknown <moatazmamdouh.js@gmail.com>
2025-12-21 13:49:09 +0200

    added noise effect to background of not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,15 @@ export default function NotFound() {
     return (<>
 
         <main>
-            <div>Background</div>
+            <div className="absolute inset-0 opacity-20 pointer-events-none z-0">
+                <svg className="w-full h-full">
+                    <filter id="noiseFilter">
+                        <feTurbulence type="fractalNoise" baseFrequency="0.95" numOctaves="3" stitchTiles="stitch" />
+                        <feColorMatrix type="saturate" values="0" />
+                    </filter>
+                    <rect width="100%" height="100%" filter="url(#noiseFilter)" />
+                </svg>
+            </div>
             <div>Content</div>
         </main>
     </>)
