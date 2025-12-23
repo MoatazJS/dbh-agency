29597e6fa340460444c3f4fe8e3a9f22df0677d1
unknown <moatazmamdouh.js@gmail.com>
2025-12-23 16:04:11 +0200

    Replaced custom tiktok icon with the one provided from fontawesome

--- lib/Components/LayoutComponents/Footer.tsx
+++ lib/Components/LayoutComponents/Footer.tsx
@@ -9,24 +9,15 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog";
 
-import { faBehance } from "@fortawesome/free-brands-svg-icons";
+import { faBehance, faTiktok } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-// Custom TikTok Icon since it's not in the default Lucide library
-const TikTokIcon = ({ className }: { className?: string }) => (
-    <svg
-        role="img"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className={className}
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.17-2.86-.6-4.12-1.31a8.776 8.776 0 01-1.43-1.04v7.37c-.15 4.1-3.41 7.15-7.43 7.54-1.63.15-3.37-.17-4.73-1.16-1.54-1.1-2.4-2.93-2.35-4.83.13-2.92 2.38-5.74 5.37-6.04.4-.04.81-.05 1.21-.02v4.03c-.45-.04-1.24.04-1.58.33-.91.73-.85 2.13-.19 2.92.54.67 1.5 1 2.37.75 1.1-.3 1.95-1.31 1.95-2.48l-.01-14.07c0-.05.02-.1-.01-.15z" />
-    </svg>
-);
-
 const socialLinks = [
-    { name: "TikTok", href: "#", icon: TikTokIcon },
+    {
+        name: "TikTok",
+        href: "#",
+        icon: (props: any) => <FontAwesomeIcon icon={faTiktok} {...props} />,
+    },
     { name: "Instagram", href: "#", icon: Instagram },
     { name: "Facebook", href: "#", icon: Facebook },
     { name: "YouTube", href: "#", icon: Youtube },
