439901f4cfdf1d9be4c4e69d531103b93b2229f4
unknown <moatazmamdouh.js@gmail.com>
2025-12-24 14:26:54 +0200

    imported fetchProjectsWithId function call into project-details page

--- app/(pages)/project-details/[id]/page.tsx
+++ app/(pages)/project-details/[id]/page.tsx
@@ -1,8 +1,10 @@
+"use client"
 import DetailsChallenge from "@/lib/Components/PagesComponents/DetailsComponents/DetailsChallenge";
 import DetailsConclusion from "@/lib/Components/PagesComponents/DetailsComponents/DetailsConclusion";
 import DetailsHero from "@/lib/Components/PagesComponents/DetailsComponents/DetailsHero";
 import DetailsSolution from "@/lib/Components/PagesComponents/DetailsComponents/DetailsSolution";
 import DetailsVideo from "@/lib/Components/PagesComponents/DetailsComponents/DetailsVideos";
+import { fetchProjectWithId } from "@/lib/Services/ApiServices";
 import React from "react";
 
 export default function ProjectDetails() {
